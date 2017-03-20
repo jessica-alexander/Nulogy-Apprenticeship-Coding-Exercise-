7abe22fdb4c730e860e1c13dc3b163b65e20ad08
Jessica <jhai_alex@hotmail.com>
2017-03-19 22:09:39 -0400

    added labour method, tested calculation is accurate with irb

--- Nupack.rb
+++ Nupack.rb
@@ -12,11 +12,22 @@ class Estimate
 
   def flat_rate
 
+
     return @base * 1.05
 
   end
+
+  def labour(num_of_people)
+
+    @labour = (num_of_people * 0.012) * flat_rate
+  end
+
+  
+
+
 end
 
 company1 = Estimate.new(1299.99)
 
 company1.flat_rate
+company1.labour(3)
